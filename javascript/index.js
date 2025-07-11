19c812724ef8510d30dc99f524b59aac4c996880
Janice <johnson.janice30@yahoo.co.uk>
2025-07-11 15:52:48 +0100

    Added moment.js and moment.js timezones to project

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+
+function updateTime () {
+// London
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(" .date");
+let londonTimeElement = londonElement.querySelector(" .time");
+let londonTime = moment().tz("Europe/London");
+londonDateElement.innerHTML = londonTime.format("D MMMM YYYY");
+londonTimeElement.innerHTML = londonTime.format("HH:mm:ss [<small>]A[</small>]");
+
+// New York
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(" .date");
+let newYorkTimeElement = newYorkElement.querySelector(" .time");
+let newYorkTime = moment().tz("America/New_York");
+newYorkDateElement.innerHTML =newYorkTime.format("D MMMM YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format("HH:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
